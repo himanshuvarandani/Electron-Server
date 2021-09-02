23873c5a7e5242be8b54e47f855ef42d8d165868
Himanshu <himvarandani@gmail.com>
2021-09-02 23:31:38 +0530

    Return jwt on login and add CORS middleware

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 from app import routes
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["*"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+  expose_headers=["*"],
+)
 
 @app.get("/")
 def home():
--- app/routes/student/login.py
+++ app/routes/student/login.py
@@ -1,13 +1,27 @@
 from app import app
+from datetime import datetime, timedelta
 from fastapi import Request
+import jwt
+from fastapi.responses import JSONResponse
+
+SECRET_KEY = "loadthisfromenv"
 
 @app.post("/student/login")
 async def student_login(request: Request):
   body = await request.json()
   u = body["username"]
   p = body["password"]
+
+  payload = {
+    "exp": datetime.utcnow()+timedelta(days=0,hours=24),
+    "iat": datetime.utcnow(),
+  }
+  token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
   
   if u == "USERNAME" and p == "PASSWORD":
-    return {"token": "4hbjn434jb234vjb"}
+    content = {"status": "OK"}
+    response = JSONResponse(content=content)
+    response.headers["X-AUTH-TOKEN"] = f"{token}"
+    return response
   
-  return {}
+  return {"status": "FAIL"}
