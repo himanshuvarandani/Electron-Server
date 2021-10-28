7cd5484fa874e4d13aa5a2d21114978f53e36d6a
Himanshu <himvarandani@gmail.com>
2021-10-29 00:33:28 +0530

    Use pydantic classes for request body, Closes #1

--- app/routes/teacher/login.py
+++ app/routes/teacher/login.py
@@ -1,13 +1,26 @@
+import re
 from app import app
-from fastapi import Request
+from fastapi import Response
+from pydantic import BaseModel, validator
+
+class RequestBody(BaseModel):
+  email: str
+  password: str
+
+  @validator('email')
+  def validate_email(cls, v):
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+
+    if not re.fullmatch(regex, v):
+      raise ValueError
+    return v
 
 @app.post("/teacher/login")
-async def teacher_login(request: Request):
-  body = await request.json()
-  u = body["username"]
-  p = body["password"]
+async def teacher_login(body: RequestBody, response: Response):
+  email = body.email
+  password = body.password
   
-  if u == "USERNAME" and p == "PASSWORD":
+  if email == "test@gmail.com" and password == "pass":
     return {"token": "4hbjn434jb234vjb"}
   
   return {}
