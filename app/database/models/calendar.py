f89eaf9418d2ad1f3074810f2d87b30edcd66b68
Himanshu <himvarandani@gmail.com>
2022-02-25 21:43:22 +0530

    Add calendar schema

---
+++ app/database/models/calendar.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.sql import func
+from app.database.models.base import Base
+from app.database.models.subjects import Subjects
+from app.database.models.classes import Classes
+
+
+class Calendar(Base):
+    __tablename__ = "calendar"
+    id = Column(Integer, primary_key=True)
+    subject_id = Column(Integer, ForeignKey(Subjects.id))
+    class_id = Column(Integer, ForeignKey(Classes.id))
+    deadline = Column(DateTime)
+    task = Column(String(200))
